06c8fd1dc463b7ea6b2d9bbcba03099b811d3fdd
goodboy998 <dgcoo@126.com>
2019-10-28 15:54:07 +0800

    Signed-off-by: goodboy998 <dgcoo@126.com>

---
+++ tap5d/data.js
@@ -0,0 +1,4 @@
+var ks=[
+  {name:"井字游戏",name_en:"Tic Tac Toe - OX",img:"/games/tictactoe-ad.jpg",iosid:1445644235},
+  {name:"玛雅神庙",name_en:"Temple Of Mayan",img:"/games/templeofmayan-ad.jpg",iosid:1225079351}
+];
